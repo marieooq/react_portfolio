ee39ea3e09ab8e61ab2288cf0453d73ab8ecea1b
Marie Otaki <otakimarie@gmail.com>
2019-07-09 20:26:20 -0700

    modified css file

--- src/Services.js
+++ src/Services.js
@@ -7,6 +7,37 @@ class Services extends React.Component{
       <div className="services">
         <h3>services</h3>
         <h2>What we offer</h2>
+
+        <div className="row">
+          <div>
+            <span>
+              <ion-icon name="phone-portrait"></ion-icon>
+            </span>
+            <h4>Responsive</h4>
+            <p>We create responsive design services.</p>
+          </div>
+          <div>
+            <span>
+              <ion-icon name="cube"></ion-icon>
+            </span>
+            <h4>Redesign</h4>
+            <p>Revise the design better.</p>
+          </div>
+          <div>
+            <span>
+              <ion-icon name="thumbs-up"></ion-icon>
+            </span>
+            <h4>Favorite</h4>
+            <p>Millions of users <ion-icon name="heart"></ion-icon> get started our services.</p>
+          </div>
+          <div>
+            <span>
+              <ion-icon name="help"></ion-icon>
+            </span>
+            <h4>Question</h4>
+            <p>Welcome your feedback.</p>
+          </div>
+        </div>
       </div>
     );
   }
