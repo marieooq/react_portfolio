d29980d52aa6fb3889095c423ddadbf7bfa781fd
Marie Otaki <otakimarie@gmail.com>
2019-07-19 20:18:47 -0700

    modified files

--- src/About.js
+++ src/About.js
@@ -1,11 +1,46 @@
 import React from 'react';
 import './App.css';
 import './About.css';
+import axios from "axios";
+
+// const axios = require('axios');
+
+// axios.get('https://medium.com/feed/@marie_woq')
+// .then(function (response) {
+//   console.log(response);
+// })
+// .catch(function (error) {
+//   console.log(error);
+// });
+let articles = [];
+async function getMediumFeed() {
+	try {
+		const mediumRssFeed = "https://medium.com/feed/@marie_woq";
+		const rssToJsonApi = "https://api.rss2json.com/v1/api.json";
+    const data = { params: { rss_url: mediumRssFeed } };
+    console.error("Medium RSS reader is working.");
+		return await axios.get(rssToJsonApi, data);
+	} catch (error) {
+		console.error(error.message);
+	}
+}
+
+getMediumFeed().then(res => {
+  console.log(res.data.items);
+
+}).catch(err => {
+  console.log(err);
+})
+
 
 class About extends React.Component{
   render() {
     return (
-      <div>About</div>
+      <div>
+        <h2>About</h2>
+        {articles}
+      </div>
+
     );
   }
 }
--- src/Work1.js
+++ src/Work1.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
-import Content_image1 from './img/host_family.png';
-
+import Work from './Work';
 
 class Work1 extends React.Component{
   render() {
     return (
       <div>
-        <p>Work1</p>
+        <Work title="Title1" description="Floweraura.com is an online flower & cakes store that delivers flowers, cakes and many other great gifts all over India and I got the opportunity to re-design their Desktop and Mobile website.
+          Powered by a strong affiliate network and channel stores, Floweraura is ready to offer its gifts in more than 180 cities across India.
+          Floweraura.com is an online flower & cakes store that delivers flowers, cakes and many other great gifts all over India and I got the opportunity to re-design their Desktop and Mobile website.
+          Powered by a strong affiliate network and channel stores, Floweraura is ready to offer its gifts in more than 180 cities across India." />
+        
       </div>
     );
   }
