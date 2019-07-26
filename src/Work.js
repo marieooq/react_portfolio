36bf74f896535cf33828f760b6d85cdf3f1afb22
Marie Otaki <otakimarie@gmail.com>
2019-07-26 11:52:03 -0700

    link work to work detail

--- src/Work.js
+++ src/Work.js
@@ -7,19 +7,13 @@ import { Link } from 'react-router-dom'
 class Work extends React.Component{
   render() {
     const sections = {title1: '/work1', title2: '/work2', title3: '/work3', title4: '/work4'};
-    
-    const navLinks = Object.keys(sections).map(keyName => {
-      return (
-        <li><Link to={sections[keyName]}>{keyName}</Link></li>
-      )
-    }); 
 
     return (
       <div className="container">
-        <WorkContent myTitle="Title1" myURL="#"/>
-        <WorkContent myTitle="Title2" myURL="#"/>
-        <WorkContent myTitle="Title3" myURL="#"/>
-        <WorkContent myTitle="Title4" myURL="#"/>
+        <WorkContent myTitle="Title1" myURL={sections.title1}/>
+        <WorkContent myTitle="Title2" myURL={sections.title2}/>
+        <WorkContent myTitle="Title3" myURL={sections.title3}/>
+        <WorkContent myTitle="Title4" myURL={sections.title4}/>
       </div>
     );
   }
