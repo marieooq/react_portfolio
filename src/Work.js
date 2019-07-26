c5c75eb55fb54b92df4993c643ba4b2d8b7f81d5
Marie Otaki <otakimarie@gmail.com>
2019-07-25 20:01:14 -0700

    modified Work.js visual design

--- src/Work.js
+++ src/Work.js
@@ -3,32 +3,36 @@ import './App.css';
 import './Work.css';
 import Content_image1 from './img/work1.jpg';
 
-const tags = ['HTML', 'CSS', 'JavaScript', 'React'];
 
-const tagList = tags.map((elm) =>{
-  return <li>{elm}</li>;
-});
-
-// const myStyles = {
-//   backgroundImage : `url(${Content_image1})`,
-//   height: '100vh',
-//   backgroundSize: 'cover'
-// }
 
 class Work extends React.Component{
   render() {
     return (
       <div>
-        <div className="mainImage">
-          <img src={Content_image1}></img>
-        </div>
-        <div className="work_description">
-          <h2 className="work_title">{this.props.title}</h2>
-          <p className="work_detail">{this.props.description}</p>
-          <div className="work_skills">
-            <ul className="tags">
-              {tagList}
-            </ul>
+        <div className="container">
+          <div className="content">
+            <p className="content-title">Title1</p>
+            <a href="#">
+              < img className="content-img" src={Content_image1}></img>
+            </a>
+          </div>
+          <div className="content">
+            <p className="content-title">Title2</p>
+            <a href="#">
+              < img className="content-img" src={Content_image1}></img>
+            </a>
+          </div>
+          <div className="content">
+            <p className="content-title">Title3</p>
+            <a href="#">
+              < img className="content-img" src={Content_image1}></img>
+            </a>
+          </div>
+          <div className="content">
+            <p className="content-title">Title4</p>
+            <a href="#">
+              < img className="content-img" src={Content_image1}></img>
+            </a>
           </div>
         </div>
       </div>
