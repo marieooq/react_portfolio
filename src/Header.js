fc36be1c7001a0175659b81095f4c8ec1bcdd6e3
Marie Otaki <otakimarie@gmail.com>
2019-08-05 20:47:39 -0700

    make the header resopnsive

--- src/Header.js
+++ src/Header.js
@@ -1,33 +1,25 @@
 import React from 'react';
 import Background from './img/header.jpg';
 import './Header.css';
-import { CSSTransitionGroup } from 'react-transition-group';
+// import { CSSTransitionGroup } from 'react-transition-group';
 import { Link } from 'react-router-dom'
-
-const myStyles = {
-  backgroundImage : `url(${Background})`,
-  height: '100vh',
-  backgroundSize: 'cover'
-}
 
 
 class Header extends React.Component{
   render(){
     return(
       <header>
-      
-      <CSSTransitionGroup>
-        <div className="main-img"style={myStyles}></div>
-      </CSSTransitionGroup>
+      <div className="main-img">
+        <img src={Background} alt="main-image"></img>
+      </div>
       <div className="introduction">
         <div className="introduction-inner">
-          <p>Hi, there!<br></br>
-          I am Marie Otaki, a Web developper based in Vancouver.</p>
+          <p>Hi, there! I am Marie, a Web developper based in Vancouver.</p>
           <span>Welcome to my website!<br></br></span>
           <Link to='/about'>{this.props.button}</Link>
         </div>
       </div>
-      </header>
+    </header>
     );
   }
 };
