d9490f1eb9cc4ca3f928828ee567fc7ea4378f15
Marie Otaki <otakimarie@gmail.com>
2019-08-14 19:02:28 -0700

    add helmet

--- src/index.js
+++ src/index.js
@@ -12,6 +12,8 @@ import Work3 from './Work3';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import 'font-awesome/css/font-awesome.min.css'; 
 import ScrollToTop from './ScrollToTop';
+import {Helmet} from "react-helmet";
+import OgpImage from './img/header.jpg';
 
 
 
@@ -28,6 +30,14 @@ class App extends React.Component{
           <Route exact path='/work1' component={Work1}/>
           <Route exact path='/work2' component={Work2}/>
           <Route exact path='/work3' component={Work3}/>
+          <Helmet>
+            <meta property="og:title" content="Portfolio | Marie Otaki" />
+            <meta property="og:type" content="portfolio" />
+            <meta property="og:url" content="marieotaki.com" />
+            <meta property="og:image" content={OgpImage} />
+            <meta property="og:site_name" content="Portfolio | Marie Otaki" />
+            <meta property="og:description" content="This is the portfoliosite created by Marie Otaki." />
+          </Helmet>
 
         </div>
         </ScrollToTop>
