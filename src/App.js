d38df2bea989a129749b7a7c24752c13f81b3cdf
Marie Otaki <otakimarie@gmail.com>
2019-08-15 20:18:01 -0700

    remove react-helmet and react-snap

--- src/App.js
+++ src/App.js
@@ -12,9 +12,6 @@ import Work3 from './Work3';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import 'font-awesome/css/font-awesome.min.css'; 
 import ScrollToTop from './ScrollToTop';
-// import {Helmet} from "react-helmet";
-import OgpImage from './img/header.jpg';
-import { hydrate, render } from "react-dom";
 import './App.css';
 
 
@@ -31,15 +28,6 @@ class App extends React.Component{
           <Route exact path='/work1' component={Work1}/>
           <Route exact path='/work2' component={Work2}/>
           <Route exact path='/work3' component={Work3}/>
-          {/* <Helmet>
-            <meta property="og:title" content="Marie Otaki | Portfolio" />
-            <meta property="og:type" content="website" />
-            <meta property="og:url" content="marieotaki.com" />
-            <meta property="og:image" content={OgpImage} />
-            <meta property="og:site_name" content="Marie Otaki | Portfolio" />
-            <meta property="og:description" content="This is the portfoliosite created by Marie Otaki." />
-          </Helmet> */}
-
         </div>
         </ScrollToTop>
       </Router>
