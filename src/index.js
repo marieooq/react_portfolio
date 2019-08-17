426fe11bc2b8ad5406b215e2b60733fecf82e3d1
Marie Otaki <otakimarie@gmail.com>
2019-08-17 09:31:34 -0700

    remove codes related to react-snap

--- src/index.js
+++ src/index.js
@@ -4,17 +4,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { hydrate, render } from "react-dom";
-
-  const rootElement = document.getElementById("root");
-  if (rootElement.hasChildNodes()) {
-    hydrate(<App />, rootElement);
-  } else {
-    render(<App />, rootElement);
-  }
 
   
-
   ReactDOM.render(<App/>, document.getElementById('root'));
 
   // If you want your app to work offline and load faster, you can change
