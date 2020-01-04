dc3cb7101dc330cfb8dcd860a260ef23c94acb23
Marie Otaki <otakimarie@gmail.com>
2020-01-04 15:31:55 -0800

    modify header

--- src/Header.js
+++ src/Header.js
@@ -1,29 +1,28 @@
-import React from 'react';
-import Background from './img/header.jpg';
-import './Header.css';
+import React from "react";
+import Background from "./img/header.jpg";
+import "./Header.css";
 // import { CSSTransitionGroup } from 'react-transition-group';
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
-
-class Header extends React.Component{
-  render(){
-    return(
+class Header extends React.Component {
+  render() {
+    return (
       <header>
-      <div className="main-img">
-        <img src={Background} alt="main-image"></img>
-      </div>
-      <div className="introduction">
-        <div className="introduction-inner">
-          <p>Hi, there! I am Marie, a Web developper based in Vancouver.</p>
-          <span>Welcome to my website!<br></br></span>
-          <Link to='/about'>{this.props.button}</Link>
+        <div className="main-img">
+          <img src={Background} alt="main"></img>
         </div>
-      </div>
-    </header>
+        <div className="introduction">
+          <div className="introduction-inner">
+            <h1>
+              <span>Marie Otaki</span> is a software developer based in
+              Vancouver.
+            </h1>
+            {/* <Link to="/about">{this.props.button}</Link> */}
+          </div>
+        </div>
+      </header>
     );
   }
-};
-
-
+}
 
 export default Header;
