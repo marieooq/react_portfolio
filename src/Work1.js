475ddde2f07ee4cdb5ac1ce8894215f1dde83d81
Marie Otaki <otakimarie@gmail.com>
2020-01-07 11:44:50 -0800

    modify WorkTemplate

--- src/Work1.js
+++ src/Work1.js
@@ -10,19 +10,38 @@ const tagList = tags.map(elm => {
   return <li>{elm}</li>;
 });
 
+const workObj = {
+  img: { Content_image1 },
+  title: "React based To-Do List",
+  subTitle: "A simple to Do List using React.js and Firebase",
+  description:
+    "This is a simple to Do List using React.js and Firebase, which enables users to add notes and remove those. Since it uses Firebase the data will be preserved if they refresh it. It’s very intuitive design and easy to manipulate. I posted an article on Medium about what I struggled creating this app. Please check it out if you’d like to know more about it.",
+  tags: { tagList },
+  howItWorks:
+    "This is a simple to Do List using React.js and Firebase, which enables users to add notes and remove those. Since it uses Firebase the data will be preserved if they refresh it. It’s very intuitive design and easy to manipulate. I posted an article on Medium about what I struggled creating this app. Please check it out if you’d like to know more about it.",
+  difficultPart:
+    "This is a simple to Do List using React.js and Firebase, which enables users to add notes and remove those. Since it uses Firebase the data will be preserved if they refresh it. It’s very intuitive design and easy to manipulate. I posted an article on Medium about what I struggled creating this app. Please check it out if you’d like to know more about it.",
+  problemSolving:
+    "This is a simple to Do List using React.js and Firebase, which enables users to add notes and remove those. Since it uses Firebase the data will be preserved if they refresh it. It’s very intuitive design and easy to manipulate. I posted an article on Medium about what I struggled creating this app. Please check it out if you’d like to know more about it.",
+  url:
+    "https://medium.com/@marie_woq/i-dont-know-what-react-exactly-is-although-i-ve-created-an-app-using-it-e0aaf7dcabc5",
+  url_title: "Medium"
+};
+
 class Work1 extends React.Component {
   render() {
     return (
       <div>
         <Header section="work" imgSrc={Content_image1} />
         <WorkTemplate
-          img={Content_image1}
-          title="React based To-Do List"
-          subTitle="A simple to Do List using React.js and Firebase"
-          description="This is a simple to Do List using React.js and Firebase, which enables users to add notes and remove those. Since it uses Firebase the data will be preserved if they refresh it. It’s very intuitive design and easy to manipulate. I posted an article on Medium about what I struggled creating this app. Please check it out if you’d like to know more about it."
-          url="https://medium.com/@marie_woq/i-dont-know-what-react-exactly-is-although-i-ve-created-an-app-using-it-e0aaf7dcabc5"
-          url_title="Medium"
+          img={workObj.img}
+          title={workObj.title}
+          subTitle={workObj.subTitle}
           tags={tagList}
+          description={workObj.description}
+          howItWorksDescription={workObj.howItWorks}
+          difficultPartDescription={workObj.difficultPart}
+          problemSolvingDescription={workObj.problemSolving}
         />
       </div>
     );
--- src/WorkTemplate.js
+++ src/WorkTemplate.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import "./WorkTemplate.css";
+import "./WorkTemplate.scss";
 
 class WorkTemplate extends React.Component {
   render() {
@@ -9,15 +9,29 @@ class WorkTemplate extends React.Component {
         <div className="work-inner">
           <h1 className="work_title">{this.props.title}</h1>
           <p className="work-subTitle">{this.props.subTitle}</p>
-
           <p className="work-description">{this.props.description}</p>
+
+          <div className="work_skills">
+            <ul className="tags">{this.props.tags}</ul>
+          </div>
+
+          <h2 className="headline">How it works</h2>
+          <p className="headline-content">{this.props.howItWorksDescription}</p>
+
+          <h2 className="headline">The most deifficult part</h2>
+          <p className="headline-content">
+            {this.props.difficultPartDescription}
+          </p>
+
+          <h2 className="headline">How to solve the problems</h2>
+          <p className="headline-content">
+            {this.props.problemSolvingDescription}
+          </p>
+
           <div className="work_links">
             <a href={this.props.url} target="_blank" rel="noopener noreferrer">
               {this.props.url_title}
             </a>
-          </div>
-          <div className="work_skills">
-            <ul className="tags">{this.props.tags}</ul>
           </div>
         </div>
       </div>
