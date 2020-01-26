04d2a0bd9a33fe18afc7a9215b814e4668e0a5dc
Marie Otaki <otakimarie@gmail.com>
2020-01-26 12:19:30 -0800

    modify the description of React blog

--- src/WorkReactBlog.js
+++ src/WorkReactBlog.js
@@ -18,9 +18,7 @@ const imageList = images.map(elm => {
 });
 
 const relatedWebsites = {
-  "Visit the website": "https://reactnote-93fb1.web.app/",
-  Medium:
-    "https://medium.com/@marieotaki/i-dont-know-what-react-exactly-is-although-i-ve-created-an-app-using-it-e0aaf7dcabc5"
+  "Visit the website": "https://vanstudents.com/"
 };
 
 const relatedWebsitesLists = Object.keys(relatedWebsites).map(keyName => {
@@ -39,26 +37,24 @@ const relatedWebsitesLists = Object.keys(relatedWebsites).map(keyName => {
 
 const workObj = {
   img: { HeaderImage },
-  title: "React based To-Do List",
+  url: "https://vanstudents.com/",
+  title: "React based blog",
   description:
-    "This is a simple to do list using React.js and Firebase, which enables users to add notes and remove those. Since it uses Firebase the data will be preserved  even if it is refleshed. It is intuitive design and easy to manipulate. I posted an article on Medium about what I struggled creating this app. Please check it out if you’d like to know this project in detail.(The link is on the bottom of this page.)",
+    "A blog introducing life in Vancouver to Japanese visitors. It is using React.js and Contentful API.",
   tags: { tagList },
   howItWorks:
-    "This a web app is using React.js for the frontend side, and Firebase for the backend side. When you type your task (anything you want) in the text box and press the add button it’ll show up as a to-do list. It is going to be registered in the database on Firebase as well. It is going to disappear when you press the “x” button on the upper right of each task, so does it on the Firebase as well. Since your tasks are preserved on the database in real-time they are going to be as they were, even if you refresh the browser.",
+    "Since this blog is using Contentful API (headless CMS) it fetch the published data from the API and display them. If you tap the each article you can read more detail.",
   difficultPart:
-    "This is exactly my first project using React.js. Although I wrote codes looking at the tutorial, to be honest, I didn’t understand completely what React is after finishing the tutorial. So, I set my goal to that at least I figure the structure out.",
+    "It is needed to jump to a page that displays an individual article when the user tap the article link on the home page. It was a bit challenging for me because I display a certain component when jumping to another link.",
   problemSolving:
-    "I worked on modifying visual design after completing the tutorial so that I could understand the structure. That was useful for me to grasp the structure, especially the concept of React components. I inspected the code by a developer tool and identified the component I’d like to modify, then modify each CSS file related to the component.",
-  url:
-    "https://medium.com/@marie_woq/i-dont-know-what-react-exactly-is-although-i-ve-created-an-app-using-it-e0aaf7dcabc5",
-  url_title: "Medium"
+    "I used URL Parameters in React router to solve the problem. Like “/article/:slug”, providing a certain slug under the article it enables to direct to a specific article page."
 };
 
 class WorkReactBlog extends React.Component {
   render() {
     return (
       <div>
-        <Header section="work" imgSrc={HeaderImage} url="https://msgif.net/" />
+        <Header section="work" imgSrc={HeaderImage} url={workObj.url} />
         <WorkTemplate
           title={workObj.title}
           subTitle={workObj.subTitle}
