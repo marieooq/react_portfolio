230c7adb0c410023c9937dbb6a0433b468c5d683
Marie Otaki <otakimarie@gmail.com>
2020-02-08 23:31:21 -0800

    modify the url of resume

--- src/About.js
+++ src/About.js
@@ -1,10 +1,10 @@
-import React from "react";
-import Header from "./Header";
-import aboutImage from "./img/about_photo.png";
-import "./App.css";
-import "./About.scss";
-import axios from "axios";
-import { Link } from "react-router-dom";
+import React from 'react';
+import Header from './Header';
+import aboutImage from './img/about_photo.png';
+import './App.css';
+import './About.scss';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 // import { isTSEnumDeclaration } from "@babel/types";
 
 class About extends React.Component {
@@ -13,8 +13,8 @@ class About extends React.Component {
   };
 
   componentDidMount() {
-    const mediumRssFeed = "https://medium.com/feed/@marieotaki";
-    const rssToJsonApi = "https://api.rss2json.com/v1/api.json";
+    const mediumRssFeed = 'https://medium.com/feed/@marieotaki';
+    const rssToJsonApi = 'https://api.rss2json.com/v1/api.json';
     const data = { params: { rss_url: mediumRssFeed } };
     axios.get(rssToJsonApi, data).then(res => {
       this.setState({ articles: res.data.items });
@@ -23,10 +23,10 @@ class About extends React.Component {
 
   render() {
     const links = {
-      Resume: "/MarieOtaki-resume.pdf",
-      Email: "mailto:otakimarie@gmail.com",
-      LinkedIn: "https://www.linkedin.com/in/marie-otaki-a67773186/",
-      GitHub: "https://github.com/marieooq"
+      Resume: '/MarieOtaki_resume.pdf',
+      Email: 'mailto:otakimarie@gmail.com',
+      LinkedIn: 'https://www.linkedin.com/in/marie-otaki-a67773186/',
+      GitHub: 'https://github.com/marieooq'
     };
 
     const snsLinks = Object.keys(links).map(keyName => {
@@ -43,42 +43,42 @@ class About extends React.Component {
       development: {
         emoji: (
           <span role="img" aria-label="laptop">
-            💻{" "}
+            💻{' '}
           </span>
         ),
-        headline: "Software development",
+        headline: 'Software development',
         text:
-          "I’m developing web apps using HTML5, CSS3, SCSS, JavaScript, React.js, and Redux. Especially I’ve been putting much effort into developing with React. I keep brushing up my developing skills."
+          'I’m developing web apps using HTML5, CSS3, SCSS, JavaScript, React.js, and Redux. Especially I’ve been putting much effort into developing with React. I keep brushing up my developing skills.'
       },
       idea: {
         emoji: (
           <span role="img" aria-label="note">
-            🗒{" "}
+            🗒{' '}
           </span>
         ),
-        headline: "Thinking new ideas",
+        headline: 'Thinking new ideas',
         text:
           "I always take notes whatever grabs my attention, which make me come up with new idea for web apps. It also useful for solving problems even if it's a tiny one. Everything around me is hints for solving problems."
       },
       cooking: {
         emoji: (
           <span role="img" aria-label="fry-pan">
-            🍳{" "}
+            🍳{' '}
           </span>
         ),
-        headline: "Cooking",
+        headline: 'Cooking',
         text:
-          "I’m a good cooker. I can make a variety of food within limited ingredients. It’s like a puzzle. I love to try new taste so every time I face new meal I can get new hint to my recipe lists. The way of thinking helps me when I am coding as well.  "
+          'I’m a good cooker. I can make a variety of food within limited ingredients. It’s like a puzzle. I love to try new taste so every time I face new meal I can get new hint to my recipe lists. The way of thinking helps me when I am coding as well.  '
       },
       traveling: {
         emoji: (
           <span role="img" aria-label="globe">
-            🌎{" "}
+            🌎{' '}
           </span>
         ),
-        headline: "Traveling",
+        headline: 'Traveling',
         text:
-          "Love to off to new world! Of all the countries I have visited, Laos and Vietnam are  my favorite! "
+          'Love to off to new world! Of all the countries I have visited, Laos and Vietnam are  my favorite! '
       }
     };
 
@@ -103,7 +103,7 @@ class About extends React.Component {
               <section className="about-introduction">
                 <h1>
                   <span role="img" aria-label="wave">
-                    👋🏼{" "}
+                    👋🏼{' '}
                   </span>
                   I love to think about new ideas and solve problems. Let's talk
                   about topics like...
--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,10 @@
-import React from "react";
-import SpeechBubble from "./img/speech_bubble.gif";
-import Background from "./img/header.jpg";
-import "./Header.scss";
+import React from 'react';
+import SpeechBubble from './img/speech_bubble.gif';
+import Background from './img/header.jpg';
+import './Header.scss';
 
 const Header = props => {
-  if (props.section === "header") {
+  if (props.section === 'header') {
     return (
       <header>
         <div className="main-img">
@@ -20,7 +20,7 @@ const Header = props => {
         <div className="introduction">
           <div className="introduction-inner">
             <h1>
-              <span id="name">Marie Otaki</span> is a{" "}
+              <span id="name">Marie Otaki</span> is a{' '}
               <span id="job-title">Front-end developer</span> based in
               Vancouver.
             </h1>
@@ -28,13 +28,13 @@ const Header = props => {
         </div>
       </header>
     );
-  } else if (props.section === "about") {
+  } else if (props.section === 'about') {
     return (
       <header className="about-header">
         <img src={props.imgSrc} alt="about" id="about_photo" />
       </header>
     );
-  } else if (props.section === "work") {
+  } else if (props.section === 'work') {
     return (
       <header className="work-header">
         <a href={props.url} target="_blank" rel="noopener noreferrer">
@@ -42,7 +42,7 @@ const Header = props => {
         </a>
       </header>
     );
-  } else if (props.section === "work_ph") {
+  } else if (props.section === 'work_ph') {
     return (
       <header className="work-ph-header">
         <a
@@ -60,14 +60,13 @@ const Header = props => {
             <img
               src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=183178&theme=light"
               alt="Msgif - A GIF generator that converts your message into a GIF | Product Hunt Embed"
-              style={{ width: "200px" }}
             />
           </a>
           <img src={props.imgSrc} alt="work" id="work-ph-photo"></img>
         </a>
       </header>
     );
-  } else if (props.section === "blog") {
+  } else if (props.section === 'blog') {
     return (
       <header className="work-header">
         <a href={props.url} target="_blank" rel="noopener noreferrer">
