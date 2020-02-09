218e561e07707f787acbf9074ec3bf488f482742
Marie Otaki <otakimarie@gmail.com>
2020-02-09 11:45:48 -0800

    add github link to work pages

--- src/WorkMsgif.js
+++ src/WorkMsgif.js
@@ -1,11 +1,11 @@
-import React from "react";
-import "./App.css";
-import Header from "./Header";
-import WorkTemplate from "./WorkTemplate";
-import HeaderImage from "./img/msgif_header.png";
-import ContentImage from "./img/msgif_demo.gif";
+import React from 'react';
+import './App.css';
+import Header from './Header';
+import WorkTemplate from './WorkTemplate';
+import HeaderImage from './img/msgif_header.png';
+import ContentImage from './img/msgif_demo.gif';
 
-const tags = ["HTML5", "CSS3", "JavaScript", "React.js", "Redux"];
+const tags = ['HTML5', 'CSS3', 'JavaScript', 'React.js', 'Redux'];
 
 const images = [ContentImage];
 
@@ -18,7 +18,9 @@ const imageList = images.map(elm => {
 });
 
 const relatedWebsites = {
-  "Visit the website": "https://msgif.net/"
+  'Visit the website': 'https://msgif.net/',
+  GitHub: 'https://github.com/marieooq/react-msgif',
+  'Product Hunt': 'https://www.producthunt.com/posts/msgif'
 };
 
 const relatedWebsitesLists = Object.keys(relatedWebsites).map(keyName => {
@@ -37,17 +39,17 @@ const relatedWebsitesLists = Object.keys(relatedWebsites).map(keyName => {
 
 const workObj = {
   img: { HeaderImage },
-  url: "https://msgif.net/",
-  title: "Msgif",
+  url: 'https://msgif.net/',
+  title: 'Msgif',
   description:
-    "Msgif is consists of two words, “message” and “gif”. As its name suggests this is a GIF generator that converts a message typed by user into a GIF animation. I use React.js and Redux to manage components. To capture the text area in which the message is typed I used a library, “html2canvas”. In order to convert the captured images  into Gif animation I used another library,”jsgif”. ",
+    'Msgif is consists of two words, “message” and “gif”. As its name suggests this is a GIF generator that converts a message typed by user into a GIF animation. I use React.js and Redux to manage components. To capture the text area in which the message is typed I used a library, “html2canvas”. In order to convert the captured images  into Gif animation I used another library,”jsgif”. ',
   tags: { tagList },
   howItWorks:
-    "Firstly you can choose the size of the text area and style of design. You can check if the style is what you expected by typing letters in the text area before pressing the record button. After pressing the record button you can type your original message and it will be recorded. After that, press the createGIF button. You can download the GIF file in a few seconds. Every time you type a letter, html2canvas capture the text area, and it is pushed into an array. After pressing the createGIF button, jsgif converts the images in the array into a GIF animation.",
+    'Firstly you can choose the size of the text area and style of design. You can check if the style is what you expected by typing letters in the text area before pressing the record button. After pressing the record button you can type your original message and it will be recorded. After that, press the createGIF button. You can download the GIF file in a few seconds. Every time you type a letter, html2canvas capture the text area, and it is pushed into an array. After pressing the createGIF button, jsgif converts the images in the array into a GIF animation.',
   difficultPart:
-    "There are several difficult parts for me. I’ve picked up three out of them.First part is Redux part. Although I didn’t use redux at first as there are a lot of components and multiple components uses the same state I decided to use redux in the end. That was my first time to use Redux so it was confusing to understand the concept of Redux.The rest of difficult part is related to UI design. One of them is adjusting the size of text area size. There are two options for the size of text area(for twitter and for other social medias) and also the size should be fit to the size of browser. In short, the size of text area should be changed according to type of the screen and the size of browser.Third point is also related to the text area. For the beginning there were two screen on this app. One is the basic text area that user can type letters. The other one is display the letter user has typed, but it gives impression that the user interface is not organized.",
+    'There are several difficult parts for me. I’ve picked up three out of them.First part is Redux part. Although I didn’t use redux at first as there are a lot of components and multiple components uses the same state I decided to use redux in the end. That was my first time to use Redux so it was confusing to understand the concept of Redux.The rest of difficult part is related to UI design. One of them is adjusting the size of text area size. There are two options for the size of text area(for twitter and for other social medias) and also the size should be fit to the size of browser. In short, the size of text area should be changed according to type of the screen and the size of browser.Third point is also related to the text area. For the beginning there were two screen on this app. One is the basic text area that user can type letters. The other one is display the letter user has typed, but it gives impression that the user interface is not organized.',
   problemSolving:
-    "I followed the  hands-on official Redux tutorial so that I can grasp the concept of Redux. I felt it was still hard to understand the relation to action and reducer so I asked my tutor to make the minimum model about it. Following the model  gradually I come to understand the concept.Using window.matchMedia and managing the state of screen type, I was able to adjust the size of text area.I also fixed the dual screen problem using HTML5 contentEditable attribute.Using that attribute enables to type letters into a normal div element and apply CSS to the element."
+    'I followed the  hands-on official Redux tutorial so that I can grasp the concept of Redux. I felt it was still hard to understand the relation to action and reducer so I asked my tutor to make the minimum model about it. Following the model  gradually I come to understand the concept.Using window.matchMedia and managing the state of screen type, I was able to adjust the size of text area.I also fixed the dual screen problem using HTML5 contentEditable attribute.Using that attribute enables to type letters into a normal div element and apply CSS to the element.'
 };
 
 class WorkMsgif extends React.Component {
--- src/WorkPhotomap.js
+++ src/WorkPhotomap.js
@@ -1,12 +1,12 @@
-import React from "react";
-import "./App.css";
-import Header from "./Header";
-import WorkTemplate from "./WorkTemplate";
-import HeaderImage from "./img/photomap_header.png";
-import ContentImage1 from "./img/photomap_demo1.png";
-import ContentImage2 from "./img/photomap_demo2.png";
+import React from 'react';
+import './App.css';
+import Header from './Header';
+import WorkTemplate from './WorkTemplate';
+import HeaderImage from './img/photomap_header.png';
+import ContentImage1 from './img/photomap_demo1.png';
+import ContentImage2 from './img/photomap_demo2.png';
 
-const tags = ["HTML5", "CSS3", "JavaScript", "React.js", "Node.js"];
+const tags = ['HTML5', 'CSS3', 'JavaScript', 'React.js', 'Node.js'];
 
 const images = [ContentImage1, ContentImage2];
 
@@ -19,7 +19,8 @@ const imageList = images.map(elm => {
 });
 
 const relatedWebsites = {
-  "Visit the website": "https://photos-mapping.web.app/"
+  'Visit the website': 'https://photos-mapping.web.app/',
+  GitHub: 'https://github.com/marieooq/react-photomap'
 };
 
 const relatedWebsitesLists = Object.keys(relatedWebsites).map(keyName => {
@@ -38,17 +39,17 @@ const relatedWebsitesLists = Object.keys(relatedWebsites).map(keyName => {
 
 const workObj = {
   img: { HeaderImage },
-  url: "https://photos-mapping.web.app/",
-  title: "Photomap",
+  url: 'https://photos-mapping.web.app/',
+  title: 'Photomap',
   description:
-    "This app enables to locate the photos of my twitter timeline on the world map. It uses twitter API to get data like image, location and published time, To show those data on the map it uses Mapbox API as well. Front-end side is used React.js and back-end side is used Node.js and Firebase. ",
+    'This app enables to locate the photos of my twitter timeline on the world map. It uses twitter API to get data like image, location and published time, To show those data on the map it uses Mapbox API as well. Front-end side is used React.js and back-end side is used Node.js and Firebase. ',
   tags: { tagList },
   howItWorks:
-    "Note: This app fetches the photos data from only my  own timeline for now. I’m planning to update it so that everyone can use it. Once I post a photo with location data on my twitter timeline, this app is going to fetch the data and display it on the map.If you tap the each photo, you can find detail information.",
+    'Note: This app fetches the photos data from only my  own timeline for now. I’m planning to update it so that everyone can use it. Once I post a photo with location data on my twitter timeline, this app is going to fetch the data and display it on the map.If you tap the each photo, you can find detail information.',
   difficultPart:
-    "At the beginning, I used search method in Twitter API that I can fetch the timeline data if it has a certain hashtag. However there is limitation that the API provides the data with hashtag if it was posted within 7 days. Another problem is when I hit the endpoint of Twitter API from client side, there was a CORS problem.",
+    'At the beginning, I used search method in Twitter API that I can fetch the timeline data if it has a certain hashtag. However there is limitation that the API provides the data with hashtag if it was posted within 7 days. Another problem is when I hit the endpoint of Twitter API from client side, there was a CORS problem.',
   problemSolving:
-    "I gave up the idea to filter the timeline data with a hash tag. I fetch the data if it has location data instead.To solve the CORS issue I used Express, the framework of Node,js. More specifically, I used api.use(cors())."
+    'I gave up the idea to filter the timeline data with a hash tag. I fetch the data if it has location data instead.To solve the CORS issue I used Express, the framework of Node,js. More specifically, I used api.use(cors()).'
 };
 
 class WorkPhotomap extends React.Component {
