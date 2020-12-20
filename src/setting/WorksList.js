1ab76da1f73edfa095921be92f34ec34a8640e08
Marie Otaki <otakimarie@gmail.com>
2020-12-20 15:56:45 -0800

    add featured properties and change the order of the projects

--- src/setting/WorksList.js
+++ src/setting/WorksList.js
@@ -1,4 +1,3 @@
-/* eslint-disable react/jsx-no-comment-textnodes */
 import Hoikupedia from '../img/hoikupedia.png';
 import YouTubeClone from '../img/youtube_clone.png';
 import AYACancdr_image from '../img/aya_cancer.png';
@@ -19,6 +18,17 @@ export default [
         'A YouTube clone application using React. You can search videos by keywords and play the video that you choose on this platform.',
       url: '/works/youtube-clone',
       project: 'personal',
+      featured: true
+    },
+    {
+      image: MessageGif_image,
+      title: 'Msgif',
+      stack: ['React', 'Redux', 'Sass'],
+      gitHub: 'https://github.com/marieooq/react-msgif',
+      description: 'A gif generator that converts your message into a GIF animation.Once you type your message and press a button your message turns into a GIF animation. I launched it on ProductHunt. It got 328 votes as of December 20, 2020.',
+      url: '/works/msgif',
+      project: 'personal',
+      featured: true,
     },
     {
       image: Hoikupedia,
@@ -28,7 +38,8 @@ export default [
        'Hoikupedia is an owned media for those who want to be a nanny in Vancouver managed by COS Educational Consulting, Inc. I designed and coded new webpages using HTML, CSS, JavaScript, WordPress, Figma, and Adobe Illustrator for it.'
       ),
       url: '/works/cos',
-      project: 'company'
+      project: 'company',
+      featured: true
     },
     {
       image: AYACancdr_image,
@@ -38,7 +49,8 @@ export default [
        'Built a Slack-like community service for people under the age of 39 with cancer (AYA: Adolescent and Young Adult) using Mattermost, an OSS. I was responsible for building a mobile app using React-native.'
       ),
       url: '/works/aya-cancer',
-      project: 'company'
+      project: 'company',
+      featured: true
     },
     {
       image: GreenSpoon_image,
@@ -48,17 +60,9 @@ export default [
        'Greenspoon Inc. is a Japanese startup company that offers high-quality fresh smoothies via e-commerce. I built a payment emailing system using PHP Laravel, HTML, and CSS'
       ),
       url: '/works/green-spoon',
-      project: 'company'
+      project: 'company',
+      featured: true
     },
-    {
-        image: MessageGif_image,
-        title: 'Msgif',
-        stack: ['React', 'Redux', 'Sass'],
-        gitHub: 'https://github.com/marieooq/react-msgif',
-        description: 'A gif generator that converts your message into a GIF animation.Once you type your message and press a button your message turns into a GIF animation. I launched it on ProductHunt. It got 328 votes as of December 20, 2020.',
-        url: '/works/msgif',
-        project: 'personal'
-      },
     {
       image: StockPrice_image,
       title: 'Stock Price List',
@@ -67,7 +71,8 @@ export default [
       description:
         'A simple SPA application that enables to display the stock symbol, the latest price and the desctiption. Once you type in the symbol and hit the enter or press the search button, these information show up.',
       url: '/works/stock-price',
-      project: 'personal'
+      project: 'personal',
+      featured: true
     },
     {
       image: CrudBlog_image,
@@ -77,7 +82,8 @@ export default [
       description:
         'A CRUD blog application using React, Node.js, and MongoDB. It enables users to create, read, update, and delete the post of the blog.',
       url: '/works/crud-blog',
-      project: 'personal'
+      project: 'personal',
+      featured: true
     },
     {
       image: Photomap_image,
@@ -86,7 +92,8 @@ export default [
       gitHub: 'https://github.com/marieooq/react-photomap-public',
       description:
         'This app enables to locate the photos of my twitter timeline on the world map. It uses twitter API to get data like image, location and published time.',
-      url: '/works/photomap'
+      url: '/works/photomap',
+      featured: false
     },
     {
       image: VanstudentBlog_image,
@@ -96,7 +103,8 @@ export default [
       description:
         'A blog introducing life in Vancouver to Japanese visitors. It is using React and Contentful API. ',
       url: '/works/react-blog',
-      project: 'personal'
+      project: 'personal',
+      featured: false,
     },
     {
         image: ReactNote_image,
@@ -106,6 +114,7 @@ export default [
         description:
           'This is a simple to Do List using React and Firebase, which enables users to add notes and remove those. Since it uses Firebase the data will be preserved if they refresh it. It’s very intuitive design and easy to manipulate. ',
         url: '/works/react-note',
-        project: 'personal'
+        project: 'personal',
+        featured: false
       }
   ];
