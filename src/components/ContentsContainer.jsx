382a008ca70ebdd24aac052887c7fa7be3fdb0d9
Marie Otaki <otakimarie@gmail.com>
2020-12-20 17:43:35 -0800

    add view more works link to the home page

--- src/components/ContentsContainer.jsx
+++ src/components/ContentsContainer.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import Content from './Content';
+import { Link, useLocation } from 'react-router-dom';
 import './ContentsContainer.css';
 
-const ContentsContainer = props => {
+const ContentsContainer = (props) => {
+  let location = useLocation();
   const contentLists = props.obj.map((obj, index) => {
-    return (
-      <Content 
-        contentObj = {obj}
-        key = {index}
-      />
-    );
+    return <Content contentObj={obj} key={index} />;
   });
   return (
     <div className="contents_wrapper">
-      <div className="contents-inner">{contentLists}</div>
+      <div className="contents-inner">
+        {contentLists}
+        {location.pathname === '/' ? (
+          <div className="view_more">
+            <Link to="/works">View more works</Link>
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
     </div>
   );
 };
