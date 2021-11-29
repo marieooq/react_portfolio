f2ca4e19c8c817b8e83ab1a7cf1cd79396ad3ae8
marieooq <otakimarie@gmail.com>
2021-11-28 17:12:01 -0800

    restructure src/features/works

--- src/features/works/index.js
+++ src/features/works/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Layout from "../../components/Layout/Layout";
-import worksImage from "../../assets/works.png";
+import worksImage from "./assets/works.png";
 import "./Works.css";
 import ContentsContainer from "../../components/ContentContainer/ContentsContainer";
 import worksList from "../../setting/worksList";
